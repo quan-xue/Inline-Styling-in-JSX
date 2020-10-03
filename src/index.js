e0e017b8a7f2c083b8f146a93f6b6ae5c931cdbf
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-03 18:32:55 +0800

    Add in-line styling CSS properties convert to camel case; attribute value in string

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//attribute camel case
+//value converted to string
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  borderColor: "green",
+  borderWidth: "1px",
+  borderStyle: "solid"
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <div>
+    <h1 style={customStyle}>Hello World!</h1>
+  </div>,
+  document.getElementById("root")
+);
